b8d7ae06f9127f5357aa727770e685227e1a293b
Leonardo Henrique <leonardo0507.business@gmail.com>
2023-04-01 13:51:56 -0300

    Created routes with React Router DOM

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,23 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import { theme, light, dark } from "./styles/theme";
 import GlobalStyle from "./styles/global";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Country from "./components/Country";
+import NotFound from "./components/NotFound.jsx";
 
 export default function App() {
     return (
-        <ThemeProvider theme={{ ...theme, colors: light }}>
-            <GlobalStyle />
-        </ThemeProvider>
+        <BrowserRouter>
+            <ThemeProvider theme={{ ...theme, colors: light }}>
+                <GlobalStyle />
+
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="country/:id" element={<Country />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </ThemeProvider>
+        </BrowserRouter>
     );
 }
---
+++ src/components/Country.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Country() {
+    return <div>Country</div>;
+}
