d66bd2ae516babd50ed7f72587b3c6f00653c646
Leonardo Henrique <leonardo0507.business@gmail.com>
2023-04-04 14:27:39 -0300

    Added toggle for dropdown the filter

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -1,24 +1,51 @@
 import React from "react";
 import { Menu, Button, Wrapper } from "./style";
 import SVGArrow from "../../assets/arrow.svg";
+import { useTheme } from "styled-components";
 
 export default function Filter() {
     const regions = ["Africa", "America", "Asia", "Europe", "Oceania"];
+    const [dropdown, setDropdown] = React.useState(false);
+    const [transition, setTransition] = React.useState(false);
+    const { transitions } = useTheme();
+    const handleDropdown = ({ currentTarget: btn }) => {
+        const attr = "data-transition";
+        const duration = transitions.global.duration;
+
+        if (!btn.hasAttribute(attr)) {
+            btn.setAttribute(attr, "");
+
+            if (!dropdown) {
+                setDropdown(true);
+                setTimeout(() => setTransition(true), 20);
+                setTimeout(() => btn.removeAttribute(attr), duration + 20);
+            } else {
+                setTransition(false)
+                setTimeout(() => {
+                    setDropdown(false);
+                    btn.removeAttribute(attr);
+                }, duration);
+            }
+        }
+    };
+    console.log(transition)
 
     return (
         <Wrapper>
-            <Button>
+            <Button
+                onClick={handleDropdown}
+                dropdown={dropdown}
+                transition={transition}
+            >
                 <span>Filter by Region</span>
 
                 <SVGArrow />
             </Button>
 
-            <Menu>
-                {regions.map(region => (
+            <Menu dropdown={dropdown} transition={transition}>
+                {regions.map((region) => (
                     <li key={region}>
-                        <button type="button">
-                            {region}
-                        </button>
+                        <button type="button">{region}</button>
                     </li>
                 ))}
             </Menu>
