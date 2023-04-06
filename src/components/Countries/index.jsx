a52323e695c4ebed6689119af7b9b7a3a7050935
Leonardo Henrique <leonardo0507.business@gmail.com>
2023-04-06 08:37:07 -0300

    Created infinite scroll for home

--- src/components/Countries/index.jsx
+++ src/components/Countries/index.jsx
@@ -12,6 +12,9 @@ export default function Countries() {
     const { setAllCountries, countries, setCountries } = 
         React.useContext(CountriesContext);
     const { request, error, loading } = useFetch();
+    const [group, setGroup] = React.useState(1);
+    const parent = React.useRef();
+    const items = 20;
 
     React.useEffect(() => {
         (async () => {
@@ -28,39 +31,56 @@ export default function Countries() {
         })();
     }, []);
 
+    React.useEffect(() => {
+        if (countries) {
+            const target = parent.current.lastChild;
+            const observer = new IntersectionObserver(([entry], observer) => {
+                if (entry.isIntersecting) {
+                    setGroup(group + 1)
+                    observer.unobserve(target);
+                };
+            }, { threshold: 1 });
+
+            if (items * group < countries.length) observer.observe(target);
+        }
+    }, [countries, group]);
+
     if (countries) {
         return (
-            <Wrapper>
-                {countries.map(({ flags, name, population, region, capital }) => (
-                    <li key={name.common}>
-                        <Link to={`country/${name.common}`}>
-                            <img 
-                                src={flags.svg} 
-                                alt="" 
-                                onLoad={({ target }) => target.style.opacity = 1} 
-                            />
-                            
-                            <Content>
-                                <H2>{name.common}</H2>
-
-                                <ul>
-                                    <li>
-                                        <span>Population: </span>
-                                        <span>{population.toLocaleString("en-US")}</span>
-                                    </li>
-                                    <li>
-                                        <span>Region: </span>
-                                        <span>{region}</span>
-                                    </li>
-                                    <li>
-                                        <span>Capital: </span>
-                                        <span>{capital ? capital.join(", ") : "None"}</span>
-                                    </li>
-                                </ul>
-                            </Content>
-                        </Link>
-                    </li>
-                ))}
+            <Wrapper ref={parent}>
+                {countries.map(({ flags, name, population, region, capital }, index) => {
+                    if (index < (items * group))
+                        return (
+                            <li key={name.common}>
+                                <Link to={`country/${name.common}`}>
+                                    <img 
+                                        src={flags.svg} 
+                                        alt="" 
+                                        onLoad={({ target }) => target.style.opacity = 1} 
+                                    />
+                                    
+                                    <Content>
+                                        <H2>{name.common}</H2>
+        
+                                        <ul>
+                                            <li>
+                                                <span>Population: </span>
+                                                <span>{population.toLocaleString("en-US")}</span>
+                                            </li>
+                                            <li>
+                                                <span>Region: </span>
+                                                <span>{region}</span>
+                                            </li>
+                                            <li>
+                                                <span>Capital: </span>
+                                                <span>{capital ? capital.join(", ") : "None"}</span>
+                                            </li>
+                                        </ul>
+                                    </Content>
+                                </Link>
+                            </li>
+                        )
+                })}
             </Wrapper>
         );
     } 
